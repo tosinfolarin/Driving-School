a3b60f93bc1ad678411cdb848f13e06d1361cb33
Theo <theo.osinfolarin@gmail.com>
2024-03-10 23:25:47 +0000

    new changes to Navbar with addition of L plate image and CSS styling

--- Driving School/src/components/Navbar.jsx
+++ Driving School/src/components/Navbar.jsx
@@ -1,18 +1,16 @@
 const Navbar = () => {
     return (  
         <nav className="navbar">
+            <div className="car-image-container">
+                <img src='/src/images/Lplate.png' alt="Learner Symbol" className="LPlate" />
+            </div>
             <h1>Trinity</h1>
             <h3>Driving School</h3>
             <div className="links">
                 <a href= "/">Home</a>
                 <a href= "/">About Me</a>
                 <a href= "/">Test Questions</a>
-                <a href= "/create" style= {{
-                    color: "white", 
-                    backgroundColor: "#ff0000",
-                    borderRadius: '8px',
-            
-                    }}>Sign In</a>
+                <a href= "/create">Sign In</a>
             </div>
         </nav>
     );
