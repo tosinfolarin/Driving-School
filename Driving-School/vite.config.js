3dc5dd44f4c7f8e3f9782f556c759ceceeaeae91
Theo <theo.osinfolarin@gmail.com>
2024-05-01 13:23:14 +0100

    images inn config

--- Driving-School/vite.config.js
+++ Driving-School/vite.config.js
@@ -10,4 +10,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    rollupOptions: {
+    external: [/src\/images\/.*/]
+    }
+}})
+
+
+
+// export default {
+//   build: {
+//     rollupOptions: {
+//     external: [/src\/images\/.*/]
+//     }
+//   }
+// }
