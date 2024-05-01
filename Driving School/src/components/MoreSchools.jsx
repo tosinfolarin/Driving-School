aad36fd18b4e8c00191108c48a4cbf83e1a7ba35
Theo <theo.osinfolarin@gmail.com>
2024-05-01 08:18:20 +0100

    apikey

--- Driving School/src/components/MoreSchools.jsx
+++ Driving School/src/components/MoreSchools.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Footer from "./Footer";
-import GoogleSearchResults from "./googleMapsApi/Googleresults";
+import GoogleSearchResults from "./googleMapsApi/googleresults";
 
 
 const MSchools = (props) => {
