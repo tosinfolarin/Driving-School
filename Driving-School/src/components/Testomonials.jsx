b27bc0d7244fc35b45e572b33a08a56197ed4696
Theo <theo.osinfolarin@gmail.com>
2024-05-01 13:46:34 +0100

    reset images

--- Driving-School/src/components/Testomonials.jsx
+++ Driving-School/src/components/Testomonials.jsx
@@ -1,18 +1,17 @@
-import PassingPic2 from 'src/images/PassingPic2.png'
-
 const Testimonials = () => {
     return ( 
         <div className= "all-testimonials">
         <div className ="testimonials" role="list">
             <div className="testimonial-item" role="listitem" aria-labelledby="testimonial1">
                 <div className="student-img-container">
-                    <img src={PassingPic2} alt='student picture' className="student-img"/>
+                    <img src='src/images/PassingPic2.png' alt='student picture' className="student-img"/>
+                    {/* <img src={PassingPic2} alt='student picture' className="student-img"/> */}
                 </div>
                 <div className="testimonial-text-container">
                     <p className="testimonial-text" id="testimonial1"> I have tried a few other local driving schools but Trinity is by far the best. I am pleased to say I was able to pass first time.</p>
                 </div>
             </div>
-
+            
             <div className="testimonial-item" role="listitem" aria-labelledby="testimonial2">
                 <div className="student-img-container">
                 <img src='src/images/PassingPic.png' alt='student picture' className="student-img"/>
--- Driving-School/vite.config.js
+++ Driving-School/vite.config.js
@@ -10,18 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-    external: [/src\/images\/.*/]
-    }
-}})
-
-
-
-// export default {
-//   build: {
-//     rollupOptions: {
-//     external: [/src\/images\/.*/]
-//     }
-//   }
-// }
+})
