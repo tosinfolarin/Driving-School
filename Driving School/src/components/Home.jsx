4e737b580877067148057d39580ffe95896eb8c4
Theo <theo.osinfolarin@gmail.com>
2024-05-01 12:52:01 +0100

    deleted

--- Driving School/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return (
-        <h3 className="slogan" aria-label="Your Road to Driving Starts Here">
-            "Your Road to Driving Starts Here"
-        </h3>
-    );
-}
-
-export default Home;
