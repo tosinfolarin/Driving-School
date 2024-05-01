5591a6156a332ec5d0edb344d24c927bd44b7d71
Theo <theo.osinfolarin@gmail.com>
2024-05-01 14:42:16 +0100

    images added to first page

--- Driving-School/src/components/Testomonials.jsx
+++ Driving-School/src/components/Testomonials.jsx
@@ -1,4 +1,9 @@
 import PassingPic2 from "@/images/PassingPic2.png"
+import PassingPic from "@/images/PassingPic.png"
+import ipassed from "@/images/ipassed.jpeg"
+import PassingPic3 from "@/images/PassingPic3.png"
+import PassingPic4  from "@/images/PassingPic4.png"
+import you from "@/images/you.jpeg"
 
 
 const Testimonials = () => {
@@ -9,7 +14,7 @@ const Testimonials = () => {
                 <div className="student-img-container">
                     <img src={PassingPic2} alt='student picture' className="student-img"/>
                     {/* src/images/PassingPic2.png */}
-                    {/* <img src={PassingPic2} alt='student picture' className="student-img"/> */}
+                    
                 </div>
                 <div className="testimonial-text-container">
                     <p className="testimonial-text" id="testimonial1"> I have tried a few other local driving schools but Trinity is by far the best. I am pleased to say I was able to pass first time.</p>
@@ -18,7 +23,8 @@ const Testimonials = () => {
 
             <div className="testimonial-item" role="listitem" aria-labelledby="testimonial2">
                 <div className="student-img-container">
-                <img src='src/images/PassingPic.png' alt='student picture' className="student-img"/>
+                <img src={PassingPic} alt='student picture' className="student-img"/>
+                {/* 'src/images/PassingPic.png' */}
                 </div>
                 <div className="testimonial-text-container">
                     <p className="testimonial-text"> Trinity provided a safe space for me to drive and feel comfortable to ask any question related to driving. I am very grateful for the support.</p>
@@ -27,7 +33,8 @@ const Testimonials = () => {
 
             <div className="testimonial-item" role="listitem" aria-labelledby="testimonial3">
                 <div className="student-img-container">
-                <img src='src/images/ipassed.jpeg' alt='student picture' className="student-img"/>
+                <img src={ipassed.jpeg} alt='student picture' className="student-img"/>
+                {/* 'src/images/ipassed.jpeg'  */}
                 </div>
                 <div className="testimonial-text-container">
                     <p className="testimonial-text"> I am very pleased with the way my driving lessons have gone. I was able to score very highly in my theory exam and passed with only 2 minor faults. </p>
@@ -38,7 +45,8 @@ const Testimonials = () => {
         <div className ="testimonials">
             <div className="testimonial-item">
                 <div className="student-img-container">
-                <img src='src/images/PassingPic3.png' alt='student picture' className="student-img"/>
+                <img src={PassingPic3} alt='student picture' className="student-img"/>
+                {/* 'src/images/PassingPic3.png' */}
                 </div>
                 <div className="testimonial-text-container">
                     <p className="testimonial-text"> Not only did Trinity help me with my practical exam, there was a lot of support when it came preparing for my theory exam as well.</p>
@@ -47,7 +55,8 @@ const Testimonials = () => {
 
             <div className="testimonial-item">
                 <div className="student-img-container">
-                <img src='src/images/PassingPic4.png' alt='student picture' className="student-img"/>
+                <img src={PassingPic4} alt='student picture' className="student-img"/>
+                {/* 'src/images/PassingPic4.png' */}
                 </div>
                 <div className="testimonial-text-container">
                     <p className="testimonial-text"> I had failed a few times with other driving schools but once I switched to Trinity, I understood the difference in quality and I was able to pass with my first attempt.</p>
@@ -56,7 +65,9 @@ const Testimonials = () => {
 
             <div className="testimonial-item">
                 <div className="student-img-container">
-                    <img src='src/images/you.jpeg' alt='student picture' className="student-img" />
+                    <img src={you} alt='student picture' className="student-img" />
+                    {/* 'src/images/you.jpeg' */}
+
                 </div>
                 <div className="testimonial-text-container">
                     <p className="testimonial-text"> BE THE NEXT SUCCESS STORY! </p>
